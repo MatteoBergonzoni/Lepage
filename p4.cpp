ea5e5c6a0ff2a1431115150541ef741590a466a5
Jachi9 <72570528+Jachi9@users.noreply.github.com>
2022-11-20 11:45:47 +0100

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -358,52 +358,8 @@ void system_solver () {
     eta = det_M_inv * ( (C[19]*p4_eff_15 - p4_eff_20*C[14])*p4_true_10 + (p4_eff_20*C[9] - C[19]*p4_eff_10)*p4_true_15 + (C[14]*p4_eff_10 - p4_eff_15*C[9])*p4_true_20 );
     }
 
-// print the values of p4 for levels from 1S to  6S for coulomb case, EFT and EFT corrected (eff)
-void p4_print () {
-    
-    for (int i = 0; i < 6; i++){
-     // Coulomb + short range interaction
-     activate_EFT = false;
-     activate_dirac = 0;
-     c = 0;
-     v_depth = -1.5;
-     nodes_wanted = i; 
-     E_mean = eigenvalue(-9,10,0,300,10000) ;
-     std::cout << i+1 <<"S (true): " << p4mean(0, range_psi[i], 30000, E_mean) << std::endl ; 
-     // Effective potential
-     v_depth = 0;
-     a = 1; 
-     c_1 = -37.05; 
-     d_1 = 0.4; 
-     activate_EFT = true;
-     nodes_wanted = i;
-     E_mean = eigenvalue(-9,10,0,300,10000);
-     double p4_eff = p4mean(0, range_psi[i], 30000, E_mean);
-     std::cout << i+1 <<"S (eff): " << p4_eff << std::endl;
-     // Correct effective potential
-     std::cout << i+1 <<"S (c_eff): " << (Z*p4_eff + Gamma*C[i] + eta*D[i]) << std::endl;  
-}
-        
-    
-   
-int main () {
-
-// INPUT FROM psi_boom_file
- std::ifstream fin("psi_boom_file.txt");
- double reader; 
- for (int i = 0; i< 20; i++) {
-     fin >> reader; 
-     fin >> range_psi[i];
-     fin >> range_psi_eff[i]; 
-     fin >> E_means[i];   
-     fin >> E_means_eff[i];
-     fin >> integral_psi_level[i];
-     fin >> integral_psi_level_eff[i]; 
-     }
-     
-
  // Fix Z, Gamma and eta using the levels 10S, 15S e 20S
- 
+void fix_constants () {
  x_initial = 0;
  x_final = 300;
  DISCRETIZATION = 10000;
@@ -450,17 +406,61 @@ int main () {
  std::cout << "\nZ = " << Z << std::endl;
  std::cout << "\nGamma = " << Gamma << std::endl;
  std::cout << "\neta = " << eta << '\n' << std::endl;
+}
+
+// print the values of p4 for levels from 1S to  6S for coulomb case, EFT and EFT corrected (eff)
+void p4_print () {
+    
+    for (int i = 0; i < 6; i++){
+     // Coulomb + short range interaction
+     activate_EFT = false;
+     activate_dirac = 0;
+     c = 0;
+     v_depth = -1.5;
+     nodes_wanted = i; 
+     E_mean = eigenvalue(-9,10,0,300,10000) ;
+     std::cout << i+1 <<"S (true): " << p4mean(0, range_psi[i], 30000, E_mean) << std::endl ; 
+     // Effective potential
+     v_depth = 0;
+     a = 1; 
+     c_1 = -37.05; 
+     d_1 = 0.4; 
+     activate_EFT = true;
+     nodes_wanted = i;
+     E_mean = eigenvalue(-9,10,0,300,10000);
+     double p4_eff = p4mean(0, range_psi[i], 30000, E_mean);
+     std::cout << i+1 <<"S (eff): " << p4_eff << std::endl;
+     // Correct effective potential
+     std::cout << i+1 <<"S (c_eff): " << (Z*p4_eff + Gamma*C[i] + eta*D[i]) << std::endl;  
+}
+        
+    
+   
+int main () {
+
+// INPUT FROM psi_boom_file
+ std::ifstream fin("psi_boom_file.txt");
+ double reader; 
+ for (int i = 0; i< 20; i++) {
+     fin >> reader; 
+     fin >> range_psi[i];
+     fin >> range_psi_eff[i]; 
+     fin >> E_means[i];   
+     fin >> E_means_eff[i];
+     fin >> integral_psi_level[i];
+     fin >> integral_psi_level_eff[i]; 
+     }
+     
+
+ fix_constants (); 
 
 /*
- // Result of the previous lines 
+ // Result of the previous line 
  Z = 0.350827;
  Gamma = -3557.78;
  eta = -2390.06;
 */
 
-
  p4_print (); 
- 
-
-
+    
  }
